1c06129559308b13f5a344bbd276acc0e4602234
pedrpan <psoto004@fiu.edu>
2019-12-05 00:27:16 -0500

    final version (tested extensively)

--- subset_tree.c
+++ subset_tree.c
@@ -135,41 +135,30 @@ unsigned uabs(int x){
 }
 
 struct tree *update_set(int key, struct tree *p, struct tree **ind){
-// struct tree *update_set(int* place, int* indices, int key, struct tree *p){
   int tree_search(int low_high[2], int key, struct tree *p);
   struct tree *remove_key(int key, struct tree *p);
   int min_max[2] = {INT_MIN, INT_MAX};
   struct tree *insert_key(int key, struct tree *p);
 
   if (tree_search(min_max, key, p) == 1){
-    // indices[*place] = key;
-    // *place = (*place)+1;
     *ind = insert_key(key, *ind);
     return remove_key(key, p);
   }
   else if (uabs(key - min_max[0]) < uabs(min_max[1] - key)){
-    // indices[*place] = min_max[0];
-    // *place = (*place)+1;
     *ind = insert_key(min_max[0], *ind);
     return remove_key(min_max[0], p);
   }
   else if (uabs(key - min_max[0]) > uabs(min_max[1] - key)){
-    // indices[*place] = min_max[1];
-    // *place = (*place)+1;
     *ind = insert_key(min_max[1], *ind);
     return remove_key(min_max[1], p);
   }
   else{
     int coin = rand() % 2;
     if (coin == 0){
-      // indices[*place] = min_max[0];
-      // *place = (*place)+1;
       *ind = insert_key(min_max[0], *ind);
       return remove_key(min_max[0], p);
     }
     else{
-      // indices[*place] = min_max[1];
-      // *place = (*place)+1;
       *ind = insert_key(min_max[1], *ind);
       return remove_key(min_max[1], p);
     }
@@ -185,25 +174,21 @@ void index_tree_to_index_array(int *place, int* ray, struct tree *p){
   }
 }
 
-void generate_batch_indices(int seed, int data_size, int batch_size, int *batch_indices){
+void generate_subsets(int seed, int set_size, int subset_size, int *subset){
   void tree_print(struct tree *p);
   struct tree *update_set(int key, struct tree *p, struct tree **ind);
-  // struct tree *update_set(int *place, int *batch_indices, int key, struct tree *p);
   void index_tree_to_index_array(int *place, int* ray, struct tree *p);
   struct tree *construct_tree(int low, int high);
   void tree_free(struct tree *p);
 
-  struct tree *potentials = construct_tree(0, data_size-1);
+  struct tree *potentials = construct_tree(0, set_size-1);
   struct tree *forbidden = NULL;
-  // srand(seed);
 
-  // int place = 0;
-  for (int k = 0; k < batch_size; k++)
-    potentials = update_set(rand() % data_size, potentials, &forbidden);
-    // potentials = update_set(&place, batch_indices, rand() % data_size, potentials);
+  for (int k = 0; k < subset_size; k++)
+    potentials = update_set(rand() % set_size, potentials, &forbidden);
 
   int place = 0;
-  index_tree_to_index_array(&place, batch_indices, forbidden);
+  index_tree_to_index_array(&place, subset, forbidden);
   tree_free(potentials);
   tree_free(forbidden);
 }
@@ -217,45 +202,37 @@ void tree_free(struct tree *p){
 }
 
 int main(int argc, char const *argv[]) {
-  void generate_batch_indices(int seed, int data_size, int batch_size, int *batch_indices);
+  clock_t start = clock();
+  void generate_subsets(int seed, int set_size, int subset_size, int *subset);
   struct tree *construct_tree(int low, int high);
   void tree_print(struct tree *p);
   void index_tree_to_index_array(int *place, int* ray, struct tree *p);
 
+  int set_size, subset_size, num_of_subsets;
+  if (argc > 3){
+    set_size = atoi(argv[1]);
+    subset_size = atoi(argv[2]);
+    num_of_subsets = atoi(argv[3]);
+  }
+  else{
+    set_size = 10000;
+    subset_size = 100;
+    num_of_subsets = 5;
+  }
+
   int seed = time(NULL);
   srand(seed);
-  int data_size = 10;
-  int batch_size = 7;
   printf("\n");
-  int *batch_indices = malloc(batch_size*sizeof(int));
-  for (int k = 0; k < 500000; k++) {
-    printf("%s\n", "gen start");
-    generate_batch_indices(seed, data_size, batch_size, batch_indices);
-    printf("Here is a random subset for n = %d, k = %d\n", data_size, batch_size);
-    for (int i = 0; i < batch_size; i++) {
-      printf("%d, ", batch_indices[i]);
-    }
-    printf("\n\n");
+  int *subset = malloc(subset_size*sizeof(int));
+  for (int k = 0; k < num_of_subsets; k++) {
+    generate_subsets(seed, set_size, subset_size, subset);    printf("{");
+    for (int i = 0; i < subset_size-1; i++)
+      printf("%d, ", subset[i]);
+    printf("%d}\n", subset[subset_size-1]);
+    printf("\n");
   }
+  clock_t end = clock();
+  printf("main() generated %d many %d-subsets of a set of size %d in\n", num_of_subsets, subset_size, set_size);
+  printf("time = %lf\n\n", ((double) (end - start)) / CLOCKS_PER_SEC);
   return 0;
 }
-
-void tree_print(struct tree *p){
-  if (p != NULL) {
-    tree_print(p->left);
-    printf("%d, ", p->key);
-    tree_print(p->right);
-  }
-}
-
-
-// int tree_count(struct tree *p){
-//   if (p != NULL) {
-//     int count = 1;
-//     count += tree_count(p->left);
-//     count += tree_count(p->right);
-//     return count;
-//   }
-//   else
-//     return 0;
-// }
